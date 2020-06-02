6edd588f48fc2ec4538bca91e3e0e8f45e743b8e
David <shogunuz@gmail.com>
2020-06-02 08:09:05 +0700

    v2.5

--- WebAPI/somefeatures/GetListOfWorkers.cs
+++ WebAPI/somefeatures/GetListOfWorkers.cs
@@ -11,44 +11,28 @@ namespace WebAPI.somefeatures
     public class GetListOfWorkers
     {
         public int NumberOfWorkers { get; private set; } // 365
-
-        //Делаю запрос в БД чтобы получить json данные о сотрудниках, отправленных на отпуск
-        private string GetStringOfH()
+        
+        public Dictionary<int, Dictionary<string, string>> GetListOfHolidays()
         {
-            string workers;
+            Dictionary<int, Dictionary<string, string>> dictionary = new Dictionary<int, Dictionary<string, string>>();
             WebRequest request = WebRequest.Create("https://localhost:44342/api/WorkerHolidays");
             WebResponse response = request.GetResponse();
             using (Stream dataStream = response.GetResponseStream())
             {
                 StreamReader reader = new StreamReader(dataStream);
-                string responseFromServer = reader.ReadToEnd();
-                workers = responseFromServer;
+                var objects = JsonConvert.DeserializeObject<List<object>>(reader.ReadToEnd());
+                var result = objects.Select(obj => JsonConvert.SerializeObject(obj)).ToArray();
+                NumberOfWorkers = result.Length;
+                JObject jsonObj;
+                for (int i = 0; i < result.Length; i++)
+                {
+                    jsonObj = JObject.Parse(result[i]);
+                    Dictionary<string, string> dictObj = jsonObj.ToObject<Dictionary<string, string>>();
+                    dictionary.Add(i, new Dictionary<string, string>(dictObj));
+                }
             }
             response.Close();
-
-            return workers;
-        }
-
-        //Обрабатываю string, отделяю пользователей и добавляю в словарь словарей.
-        private Dictionary<int, Dictionary<string, string>> CalcDictOfH(string jsonInput)
-        {
-            Dictionary<int, Dictionary<string, string>> dictionary = new Dictionary<int, Dictionary<string, string>>();
-            Console.WriteLine(jsonInput + "\n\n");
-            var objects = JsonConvert.DeserializeObject<List<object>>(jsonInput);
-            var result = objects.Select(obj => JsonConvert.SerializeObject(obj)).ToArray();
-            NumberOfWorkers = result.Length;
-            JObject jsonObj;
-            for (int i = 0; i < NumberOfWorkers; i++)
-            {
-                jsonObj = JObject.Parse(result[i]);
-                Dictionary<string, string> dictObj = jsonObj.ToObject<Dictionary<string, string>>();
-                dictionary.Add(i, new Dictionary<string, string>(dictObj));
-            }
             return dictionary;
-        }
-        public Dictionary<int, Dictionary<string, string>> GetListOfHolidays()
-        {
-            return CalcDictOfH(GetStringOfH());
         }
 
     }
